cdc4cc644988a1758e31c15b7dc352049eca51d1
Course Account <cs504306@dtn2.oscer.ou.edu>
2023-02-16 22:32:54 -0600

    run batch exp

--- hw1/batch.sh
+++ hw1/batch.sh
@@ -8,12 +8,12 @@
 # The %j is translated into the job number
 #SBATCH --output=results/hw0_%j_stdout.txt
 #SBATCH --error=results/hw0_%j_stderr.txt
-#SBATCH --time=00:10:00
+#SBATCH --time=00:20:00
 #SBATCH --job-name=hw1_test
 #SBATCH --mail-user=ikang@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504306/aml/hw1
-#SBATCH --array=0-99
+#SBATCH --array=1-159
 #
 #################################################
 
