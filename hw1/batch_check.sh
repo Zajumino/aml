a0a0a4a787addd34fdf32cbb879321c862ba519b
Course Account <cs504306@schooner2.oscer.ou.edu>
2023-02-16 22:11:59 -0600

    stuff

--- hw1/batch_check.sh
+++ hw1/batch_check.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #
-#SBATCH --partition=normal
+#SBATCH --partition=debug_5min
 #SBATCH --ntasks=1
 # memory in MB
 #SBATCH --mem=1024
 # The %j is translated into the job number
 #SBATCH --output=results/hw0_%j_stdout.txt
 #SBATCH --error=results/hw0_%j_stderr.txt
-#SBATCH --time=00:10:00
+#SBATCH --time=00:02:00
 #SBATCH --job-name=hw1_test
 #SBATCH --mail-user=ikang@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504306/aml/hw1
-#SBATCH --array=0-99
 #
 #################################################
 
@@ -22,4 +21,4 @@
 conda activate tf
 
 # run exp
-python hw1.py --exp_type 'bmi' --exp_index $SLURM_ARRAY_TASK_ID --epochs 100 --activation_hidden 'swish'
+python hw1.py --check
