6f579d9979b078121995007f49f10ddcc5e42222
I-Kang Kennedy <ikang@kennedygang.com>
2023-02-24 13:06:39 -0600

    prepare batch for part 2

--- hw2/batch.sh
+++ hw2/batch.sh
@@ -9,12 +9,12 @@
 # The %j is translated into the job number
 #SBATCH --output=results/hw2_%j_stdout.txt
 #SBATCH --error=results/hw2_%j_stderr.txt
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 #SBATCH --job-name=hw2_test
 #SBATCH --mail-user=ikang@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504306/aml/hw2
-#SBATCH --array=0-9
+#SBATCH --array=0-199
 #
 #################################################
 
@@ -23,4 +23,4 @@
 conda activate tf
 
 # run exp
-python hw2.py --exp_type 'noreg' --cpus_per_task $SLURM_CPUS_PER_TASK --exp_index $SLURM_ARRAY_TASK_ID --epochs 1000 --activation_hidden 'swish'
+python hw2.py --exp_type 'dropout' --cpus_per_task $SLURM_CPUS_PER_TASK --exp_index $SLURM_ARRAY_TASK_ID --epochs 1000 --activation_hidden 'swish'
