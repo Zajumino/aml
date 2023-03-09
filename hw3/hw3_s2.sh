f7c9415b8c67fa1d13f6f55fdaaa13f6f17c0b66
I-Kang Kennedy <ikang@kennedygang.com>
2023-03-09 17:08:30 -0600

    try more shallow for less overfitting

---
+++ hw3/hw3_s2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --partition=normal
+#SBATCH --cpus-per-task=10
+# memory in MB
+#SBATCH --mem=15000
+#SBATCH --output=results/hw3_s2_%04a_stdout.txt
+#SBATCH --error=results/hw3_s2_%04a_stderr.txt
+#SBATCH --time=04:00:00
+#SBATCH --job-name=hw3_s2
+#SBATCH --mail-user=ikang@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/cs504306/aml/hw3
+#SBATCH --array=0-4
+#
+#################################################
+# Do not change this line unless you have your own python/tensorflow/keras set up
+
+. /home/fagg/tf_setup.sh
+conda activate tf
+
+# 2023
+python hw3.py @oscer.txt @exp.txt @net_s2.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --save_model
+
